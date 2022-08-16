a800688f4438a84162f937397211e27e7389912a
Reza Lael <48585435+rezalaal@users.noreply.github.com>
2022-08-16 11:12:11 +0430

    Create variables.py

---
+++ Lessons/Lesson01/variables.py
@@ -0,0 +1,17 @@
+print(5+2)
+print(5-2)
+print(5*2)
+print(5/2)
+print(5//2)
+print(5%2)
+
+x=2
+y=3
+print(x+y)
+
+name = input("Enter your name")
+print("Hello",name)
+
+age = input("Enter your age")
+age = int(age)
+print("Your age is:",age)
